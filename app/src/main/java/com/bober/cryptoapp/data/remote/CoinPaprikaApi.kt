e6860e338562ffd8fd07cfc8279bbe4d99d92678
Bober1337IT <bartekpietras03@gmail.com>
2026-03-23 22:00:53 +0100

    Add CoinDetail domain model, CoinDetailDto, and getCoinById endpoint to CoinPaprikaApi

--- app/src/main/java/com/bober/cryptoapp/data/remote/CoinPaprikaApi.kt
+++ app/src/main/java/com/bober/cryptoapp/data/remote/CoinPaprikaApi.kt
@@ -1,12 +1,15 @@
 package com.bober.cryptoapp.data.remote
 
+import com.bober.cryptoapp.data.remote.dto.CoinDetailDto
 import com.bober.cryptoapp.data.remote.dto.CoinDto
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface CoinPaprikaApi {
 
     @GET("/v1/coins")
     suspend fun getCoins(): List<CoinDto>
 
-    
+    @GET("/v1/coins/{coinId}")
+    suspend fun getCoinById(@Path("coinId") coinId: String): CoinDetailDto
 }
---
+++ app/src/main/java/com/bober/cryptoapp/data/remote/dto/Whitepaper.kt
@@ -0,0 +1,6 @@
+package com.bober.cryptoapp.data.remote.dto
+
+data class Whitepaper(
+    val link: String,
+    val thumbnail: String
+)
