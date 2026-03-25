e7b81c85400159f0f82e0611c3ea4f7203e86a06
Bober1337IT <bartekpietras03@gmail.com>
2026-03-25 20:43:59 +0100

    Update CoinRepository and its implementation to return domain Coin models instead of DTOs

--- app/src/main/java/com/bober/cryptoapp/data/repository/CoinRepositoryImpl.kt
+++ app/src/main/java/com/bober/cryptoapp/data/repository/CoinRepositoryImpl.kt
@@ -1,8 +1,9 @@
 package com.bober.cryptoapp.data.repository
 
 import com.bober.cryptoapp.data.remote.CoinPaprikaApi
-import com.bober.cryptoapp.data.remote.dto.CoinDto
+import com.bober.cryptoapp.domain.model.Coin
 import com.bober.cryptoapp.data.remote.dto.toCoinDetail
+import com.bober.cryptoapp.data.remote.dto.toCoin
 import com.bober.cryptoapp.domain.model.CoinDetail
 import com.bober.cryptoapp.domain.repository.CoinRepository
 import javax.inject.Inject
@@ -10,8 +11,8 @@ import javax.inject.Inject
 class CoinRepositoryImpl @Inject constructor(
     private val api: CoinPaprikaApi
 ) : CoinRepository {
-    override suspend fun getCoins(): List<CoinDto> {
-        return api.getCoins()
+    override suspend fun getCoins(): List<Coin> {
+        return api.getCoins().map { it.toCoin() }
     }
 
     override suspend fun getCoinById(coinId: String): CoinDetail {
