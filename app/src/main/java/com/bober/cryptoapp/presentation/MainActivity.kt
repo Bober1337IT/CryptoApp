e23887ab48c295198cb899754f380cf6e6a31851
Bober1337IT <bartekpietras03@gmail.com>
2026-03-25 14:32:35 +0100

    Move MainActivity

---
+++ app/src/main/java/com/bober/cryptoapp/presentation/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.bober.cryptoapp.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.bober.cryptoapp.presentation.ui.theme.CryptoAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CryptoAppTheme {
+                Scaffold(modifier = Modifier.Companion.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
